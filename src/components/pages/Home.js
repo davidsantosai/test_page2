bf79bb81cb062acf55d558e0f6dd633a652693c0
David Santamaria <dsantamaria@lean-tech.io>
2022-04-06 11:19:07 -0500

    Initial commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App';
+import HeroSection from '../HeroSection';
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection/>
+    </>
+  );
+}
+
+export default Home;
