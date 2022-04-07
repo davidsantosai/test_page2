ee9bacc5e19284c8870f911277d0325d2afcfe99
David Santamaria <dsantamaria@lean-tech.io>
2022-04-07 15:21:40 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -12,7 +16,11 @@ function App() {
           <Navbar />
           <Routes>
             <Route path='/' exact element={<Home/>}/>
+            <Route path='/services' element={<Services/>}/>
+            <Route path='/products' element={<Products/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
           </Routes>
+          <Footer/>
         </Router>
       </>
   );
